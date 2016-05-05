snedigqh/angular

commit 9220eb6c5b18cab337840165fdd47d69304c259b

refactor(compiler_cli): move it into modules/@angular and integrate properly into the build
This also does no more depend on a version
on npm for the compiler_cli.

Also runs the tests for tools/metadata

tools/tsc-watch/index.ts
@@ -4,51 +4,49 @@ import {writeFileSync, mkdirSync, existsSync} from 'fs';
 export * from './tsc_watch';
 import 'reflect-metadata';
 
-const OFFLINE_COMPILE =
-    [
-      'output/output_emitter_codegen_untyped',
-      'output/output_emitter_codegen_typed',
-      'offline_compiler_codegen_untyped',
-      'offline_compiler_codegen_typed'
-    ]
+const OFFLINE_COMPILE = [
+  'output/output_emitter_codegen_untyped',
+  'output/output_emitter_codegen_typed',
+  'offline_compiler_codegen_untyped',
+  'offline_compiler_codegen_typed'
+];
 
-    function
-    processOutputEmitterCodeGen():
-        Promise<number> {
-          return new Promise((resolve, reject) => {
-                   var outDir = 'dist/all/@angular/compiler/test/';
-                   var promises = [];
-                   console.log('Processing codegen...');
-                   OFFLINE_COMPILE.forEach((file: string) => {
-                     var codegen = require('../../all/@angular/compiler/test/' + file + '.js');
-                     if (codegen.emit) {
-                       console.log(`  ${file} has changed, regenerating...`);
-                       promises.push(
-                           Promise.resolve(codegen.emit())
-                               .then((code) => { writeFileSync(outDir + file + '.ts', code); }));
-                     }
-                   });
-                   if (promises.length) {
-                     Promise.all(promises)
-                         .then(() => {
-                           var args = [
-                             '--project',
-                             'tools/cjs-jasmine/tsconfig-output_emitter_codegen.json'
-                           ];
-                           console.log('    compiling changes: tsc ' + args.join(' '));
-                           var tsc = spawn(TSC, args, {stdio: 'pipe'});
-                           tsc.stdout.on('data', (data) => process.stdout.write(data));
-                           tsc.stderr.on('data', (data) => process.stderr.write(data));
-                           tsc.on('close', (code) => code ? reject('Tsc exited with: ' + code) :
-                                                            resolve(code));
-                         })
-                         .catch(reportError);
-                   } else {
-                     resolve(0);
-                   }
-                 })
-              .catch(reportError);
-        }
+function processOutputEmitterCodeGen():
+    Promise<number> {
+      return new Promise((resolve, reject) => {
+                var outDir = 'dist/all/@angular/compiler/test/';
+                var promises = [];
+                console.log('Processing codegen...');
+                OFFLINE_COMPILE.forEach((file: string) => {
+                  var codegen = require('../../all/@angular/compiler/test/' + file + '.js');
+                  if (codegen.emit) {
+                    console.log(`  ${file} has changed, regenerating...`);
+                    promises.push(
+                        Promise.resolve(codegen.emit())
+                            .then((code) => { writeFileSync(outDir + file + '.ts', code); }));
+                  }
+                });
+                if (promises.length) {
+                  Promise.all(promises)
+                      .then(() => {
+                        var args = [
+                          '--project',
+                          'tools/cjs-jasmine/tsconfig-output_emitter_codegen.json'
+                        ];
+                        console.log('    compiling changes: tsc ' + args.join(' '));
+                        var tsc = spawn(TSC, args, {stdio: 'pipe'});
+                        tsc.stdout.on('data', (data) => process.stdout.write(data));
+                        tsc.stderr.on('data', (data) => process.stderr.write(data));
+                        tsc.on('close', (code) => code ? reject('Tsc exited with: ' + code) :
+                                                        resolve(code));
+                      })
+                      .catch(reportError);
+                } else {
+                  resolve(0);
+                }
+              })
+          .catch(reportError);
+    }
 
 function md(dir: string, folders: string[]) {
   if (folders.length) {
@@ -63,7 +61,7 @@ function md(dir: string, folders: string[]) {
 
 var tscWatch: TscWatch = null;
 var platform = process.argv.length >= 3 ? process.argv[2] : null;
-var watch: boolean = process.argv.length >= 4 ? !!process.argv[3] : false;
+var runMode: string = process.argv.length >= 4 ? process.argv[3] : null;
 
 if (platform == 'node') {
   tscWatch = new TscWatch({
@@ -73,7 +71,7 @@ if (platform == 'node') {
     complete: 'Compilation complete. Watching for file changes.',
     onChangeCmds: [
       processOutputEmitterCodeGen,
-      ['node', 'dist/tools/cjs-jasmine', '--', '{@angular,benchpress}/**/*_spec.js']
+      ['node', 'dist/tools/cjs-jasmine', '--', '{@angular,benchpress}/**/*_spec.js', '@angular/compiler_cli/test/**/*_spec.js']
     ]
   });
 } else if (platform == 'browser') {
@@ -86,6 +84,24 @@ if (platform == 'node') {
         [['node', 'node_modules/karma/bin/karma', 'start', '--no-auto-watch', 'karma-js.conf.js']],
     onChangeCmds: [['node', 'node_modules/karma/bin/karma', 'run', 'karma-js.conf.js']]
   });
+} else if (platform == 'tools') {
+  tscWatch = new TscWatch({
+    tsconfig: 'tools/tsconfig.json',
+    start: 'File change detected. Starting incremental compilation...',
+    error: 'error',
+    complete: 'Compilation complete. Watching for file changes.',
+    onChangeCmds: [
+      // TODO: fix and enable tests for public_api_spec again!
+      // ['node', 'dist/tools/cjs-jasmine/index-tools', '--', '{metadata,public_api_guard}/**/*{_,.}spec.js']
+      ['node', 'dist/tools/cjs-jasmine/index-tools', '--', 'metadata/**/*{_,.}spec.js']
+    ]
+  });
 }
 
-watch ? tscWatch.watch() : tscWatch.run();
+if (runMode === 'watch') {
+  tscWatch.watch();
+} else if (runMode === 'triggerCmds') {
+  tscWatch.triggerCmds();
+} else {
+  tscWatch.run();
+}

tools/tsc-watch/tsc_watch.ts
@@ -8,7 +8,6 @@ enum State {
 }
 
 export const TSC = 'node_modules/typescript/bin/tsc';
-export const NG2TC = 'node_modules/.bin/ng2tc';
 export type Command = (stdIn, stdErr) => Promise<number>;
 
 export class TscWatch {
@@ -40,10 +39,7 @@ export class TscWatch {
   }
 
   watch() {
-    // TODO(tbosch): We don't support ng2tc in watch mode yet,
-    // so we fall back to plain tsc. However, we don't use
-    // the preprocessing of tsickle regarding decorators...
-    var args = [this.runOnce ? NG2TC : TSC, '--project', this.tsconfig];
+    var args = [TSC, '--project', this.tsconfig];
     if (!this.runOnce) args.push('--watch');
     var tsc =
         this.runCmd(args, {}, (d) => this.consumeLine(d, false), (d) => this.consumeLine(d, true));

scripts/ci-lite/test_js.sh
@@ -15,20 +15,33 @@ source ./env.sh
 cd ../..
 
 
+echo 'travis_fold:start:test.unit.tools'
+
+# Run unit tests in tools
+node ./dist/tools/tsc-watch/ tools triggerCmds
+
+echo 'travis_fold:end:test.unit.tools'
+
+
 echo 'travis_fold:start:test.unit.node'
-gulp test.compiler_cli
 
 # Run unit tests in node
-node ./dist/tools/tsc-watch/ node
+node ./dist/tools/tsc-watch/ node triggerCmds
 
 echo 'travis_fold:end:test.unit.node'
 
 
+echo 'travis_fold:start:test.compiler_cli.node'
 
-echo 'travis_fold:start:test.unit.localChrome'
+# Run compiler_cli integration tests in node
+node dist/tools/cjs-jasmine -- @angular/compiler_cli/integrationtest/**/*_spec.js
+
+echo 'travis_fold:end:test.compiler_cli.node'
 
 # rebuild since codegen has overwritten some files.
-$(npm bin)/ng2tc -p modules/tsconfig.json
+node dist/all/@angular/compiler_cli/src/main -p modules/tsconfig.json
+
+echo 'travis_fold:start:test.unit.localChrome'
 
 # Run unit tests in local chrome
 if [[ ${TRAVIS} ]]; then

modules/@angular/compiler_cli/index.ts
@@ -1,0 +1,3 @@
+export {CodeGenerator} from './src/codegen';
+export {NodeReflectorHost} from './src/reflector_host';
+export {TsickleHost, MetadataWriterHost} from './src/compiler_host';

tools/compiler_cli/src/index.ts
@@ -1,3 +1,0 @@
-export {CodeGenerator} from './codegen';
-export {NodeReflectorHost} from './reflector_host';
-export {TsickleHost, MetadataWriterHost} from './compiler_host';

build.sh
@@ -6,7 +6,7 @@ cd `dirname $0`
 
 
 TSCONFIG=./modules/tsconfig.json
-echo "====== (all)COMPILING: \$(npm bin)/ng2tc -p ${TSCONFIG} ====="
+echo "====== (all)COMPILING: \$(npm bin)/tsc -p ${TSCONFIG} ====="
 rm -rf ./dist/all/
 mkdir ./dist/all/
 
@@ -27,8 +27,14 @@ ln -s ../../../../node_modules/angular/angular.js .
 cd -
 
 # compile ts code
-$(npm bin)/ng2tc -p ${TSCONFIG}
-
+# TODO: Right now we have a cycle in that the compiler_cli depends on Angular
+# but we need it to compile Angular.
+# The solution right now is to do 2 compilation runs.
+# Fix this by separating the metadata extraction into a separate binary that does
+# not depend on Angular.
+$(npm bin)/tsc -p ${TSCONFIG}
+NG_TC="node dist/all/@angular/compiler_cli/src/main"
+$NG_TC -p modules/tsconfig.json
 
 rm -rf ./dist/packages-dist
 
@@ -42,20 +48,20 @@ for PACKAGE in \
   http \
   router \
   router-deprecated \
-  upgrade
+  upgrade \
+  compiler_cli
 do
   SRCDIR=./modules/@angular/${PACKAGE}
   DESTDIR=./dist/packages-dist/${PACKAGE}
   UMDES6PATH=${DESTDIR}/esm/${PACKAGE}.umd.js
   UMDES5PATH=${DESTDIR}/${PACKAGE}.umd.js
 
-
   if [[ ${PACKAGE} == "router-deprecated" ]]; then
     echo "======      COMPILING: \$(npm bin)/tsc -p ${SRCDIR}/tsconfig-es5.json        ====="
     $(npm bin)/tsc -p ${SRCDIR}/tsconfig-es5.json
   else
-    echo "======      COMPILING: \$(npm bin)/ng2tc -p ${SRCDIR}/tsconfig-es5.json        ====="
-    $(npm bin)/ng2tc -p ${SRCDIR}/tsconfig-es5.json
+    echo "======      COMPILING: ${NG_TC} -p ${SRCDIR}/tsconfig-es5.json        ====="
+    $NG_TC -p ${SRCDIR}/tsconfig-es5.json
   fi
 
   cp ${SRCDIR}/package.json ${DESTDIR}/
@@ -71,42 +77,43 @@ do
     find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i '' -E 's/^( +)abstract ([[:alnum:]]+\:)/\1\2/g'
   fi
 
+  if [[ ${PACKAGE} != compiler_cli ]]; then
 
-  if [[ ${PACKAGE} == "router-deprecated" ]]; then
-    echo "====== (esm)COMPILING: \$(npm bin)/tsc -p ${SRCDIR}/tsconfig-es2015.json ====="
-    $(npm bin)/tsc -p ${SRCDIR}/tsconfig-es2015.json
-  else
-    echo "====== (esm)COMPILING: \$(npm bin)/ng2tc -p ${SRCDIR}/tsconfig-es2015.json ====="
-    $(npm bin)/ng2tc -p ${SRCDIR}/tsconfig-es2015.json
+    if [[ ${PACKAGE} == "router-deprecated" ]]; then
+      echo "====== (esm)COMPILING: \$(npm bin)/tsc -p ${SRCDIR}/tsconfig-es2015.json ====="
+      $(npm bin)/tsc -p ${SRCDIR}/tsconfig-es2015.json
+    else
+      echo "====== (esm)COMPILING: $NG_TC -p ${SRCDIR}/tsconfig-es2015.json ====="
+      $NG_TC -p ${SRCDIR}/tsconfig-es2015.json
+    fi
+
+    echo "======      BUNDLING: ${SRCDIR} ====="
+    (
+      cd  ${SRCDIR}
+      echo "..."  # here just to have grep match something and not exit with 1
+      ../../../node_modules/.bin/rollup -c rollup.config.js
+    ) 2>&1 | grep -v "as external dependency"
+
+    # workaround for https://github.com/rollup/rollup/issues/626
+    if [[ ${TRAVIS} ]]; then
+      sed -i    "s/ class exports\./ class /g" ${DESTDIR}/esm/${PACKAGE}.umd.js
+    else
+      sed -i '' "s/ class exports\./ class /g" ${DESTDIR}/esm/${PACKAGE}.umd.js
+    fi
+
+    $(npm bin)/tsc  \
+        --out ${UMDES5PATH} \
+        --target es5 \
+        --allowJs \
+        ${UMDES6PATH} \
+        modules/\@angular/manual_typings/globals.d.ts \
+        modules/\@angular/typings/es6-collections/es6-collections.d.ts \
+        modules/\@angular/typings/es6-promise/es6-promise.d.ts
+    rm ${UMDES6PATH}
+
+    cat ./modules/@angular/license-banner.txt > ${UMDES5PATH}.tmp
+    cat ${UMDES5PATH} >> ${UMDES5PATH}.tmp
+    mv ${UMDES5PATH}.tmp ${UMDES5PATH}
+
   fi
-
-
-  echo "======      BUNDLING: ${SRCDIR} ====="
-  (
-    cd  ${SRCDIR}
-    echo "..."  # here just to have grep match something and not exit with 1
-    ../../../node_modules/.bin/rollup -c rollup.config.js
-  ) 2>&1 | grep -v "as external dependency"
-
-  # workaround for https://github.com/rollup/rollup/issues/626
-  if [[ ${TRAVIS} ]]; then
-    sed -i    "s/ class exports\./ class /g" ${DESTDIR}/esm/${PACKAGE}.umd.js
-  else
-    sed -i '' "s/ class exports\./ class /g" ${DESTDIR}/esm/${PACKAGE}.umd.js
-  fi
-
-  $(npm bin)/tsc  \
-      --out ${UMDES5PATH} \
-      --target es5 \
-      --allowJs \
-      ${UMDES6PATH} \
-      modules/\@angular/manual_typings/globals.d.ts \
-      modules/\@angular/typings/es6-collections/es6-collections.d.ts \
-      modules/\@angular/typings/es6-promise/es6-promise.d.ts
-  rm ${UMDES6PATH}
-
-  cat ./modules/@angular/license-banner.txt > ${UMDES5PATH}.tmp
-  cat ${UMDES5PATH} >> ${UMDES5PATH}.tmp
-  mv ${UMDES5PATH}.tmp ${UMDES5PATH}
-
 done

karma-js.conf.js
@@ -39,6 +39,7 @@ module.exports = function(config) {
     exclude: [
       'dist/all/@angular/**/e2e_test/**',
       'dist/all/@angular/examples/**',
+      'dist/all/@angular/compiler_cli/**',
       'dist/all/angular1_router.js',
       'dist/all/@angular/platform-browser/testing/e2e_util.js'
     ],

modules/@angular/compiler/private_export.ts
@@ -1,5 +1,15 @@
 import * as selector from './src/selector';
-import * as pathUtil from './src/output/path_util';
+import * as path_util from './src/output/path_util';
+import * as metadata_resolver from './src/metadata_resolver';
+import * as html_parser from './src/html_parser';
+import * as directive_normalizer from './src/directive_normalizer';
+import * as lexer from './src/expression_parser/lexer';
+import * as parser from './src/expression_parser/parser';
+import * as template_parser from './src/template_parser';
+import * as dom_element_schema_registry from './src/schema/dom_element_schema_registry';
+import * as style_compiler from './src/style_compiler';
+import * as view_compiler from './src/view_compiler/view_compiler';
+import * as ts_emitter from './src/output/ts_emitter';
 
 export namespace __compiler_private__ {
   export type SelectorMatcher = selector.SelectorMatcher;
@@ -8,9 +18,39 @@ export namespace __compiler_private__ {
   export type CssSelector = selector.CssSelector;
   export var CssSelector = selector.CssSelector;
 
-  export type AssetUrl = pathUtil.AssetUrl;
-  export var AssetUrl = pathUtil.AssetUrl;
+  export type AssetUrl = path_util.AssetUrl;
+  export var AssetUrl = path_util.AssetUrl;
 
-  export type ImportGenerator = pathUtil.ImportGenerator;
-  export var ImportGenerator = pathUtil.ImportGenerator;
+  export type ImportGenerator = path_util.ImportGenerator;
+  export var ImportGenerator = path_util.ImportGenerator;
+
+  export type CompileMetadataResolver = metadata_resolver.CompileMetadataResolver;
+  export var CompileMetadataResolver = metadata_resolver.CompileMetadataResolver;
+
+  export type HtmlParser = html_parser.HtmlParser;
+  export var HtmlParser = html_parser.HtmlParser;
+
+  export type DirectiveNormalizer = directive_normalizer.DirectiveNormalizer;
+  export var DirectiveNormalizer = directive_normalizer.DirectiveNormalizer;
+
+  export type Lexer = lexer.Lexer;
+  export var Lexer = lexer.Lexer;
+
+  export type Parser = parser.Parser;
+  export var Parser = parser.Parser;
+
+  export type TemplateParser = template_parser.TemplateParser;
+  export var TemplateParser = template_parser.TemplateParser;
+
+  export type DomElementSchemaRegistry = dom_element_schema_registry.DomElementSchemaRegistry;
+  export var DomElementSchemaRegistry = dom_element_schema_registry.DomElementSchemaRegistry;
+
+  export type StyleCompiler = style_compiler.StyleCompiler;
+  export var StyleCompiler = style_compiler.StyleCompiler;
+
+  export type ViewCompiler = view_compiler.ViewCompiler;
+  export var ViewCompiler = view_compiler.ViewCompiler;
+
+  export type TypeScriptEmitter = ts_emitter.TypeScriptEmitter;
+  export var TypeScriptEmitter = ts_emitter.TypeScriptEmitter;
 }

commit 0b452fc9eb66fb232c1135b36a4d2c8854b755fa

chore: fail build if a command from tsc-watch fails.
This bug was introduced with 9220eb6c5b18cab337840165fdd47d69304c259b
to integrate the compiler_cli into the build properly.

Closes #8480

tools/tsc-watch/index.ts
@@ -103,8 +103,8 @@ if (platform == 'node') {
 
 if (runMode === 'watch') {
   tscWatch.watch();
-} else if (runMode === 'triggerCmds') {
-  tscWatch.triggerCmds();
+} else if (runMode === 'runCmdsOnly') {
+  tscWatch.runCmdsOnly();
 } else {
   tscWatch.run();
 }

tools/tsc-watch/tsc_watch.ts
@@ -81,6 +81,11 @@ export class TscWatch {
     this.watch();
   }
 
+  runCmdsOnly() {
+    this.runOnce = true;
+    this.triggerCmds();
+  }
+
   consumeLine(buffer: Buffer, isStdError: boolean) {
     var line = '' + buffer;
     if (contains(line, this.start)) {

scripts/ci-lite/test_js.sh
@@ -18,7 +18,7 @@ cd ../..
 echo 'travis_fold:start:test.unit.tools'
 
 # Run unit tests in tools
-node ./dist/tools/tsc-watch/ tools triggerCmds
+node ./dist/tools/tsc-watch/ tools runCmdsOnly
 
 echo 'travis_fold:end:test.unit.tools'
 
@@ -26,7 +26,7 @@ echo 'travis_fold:end:test.unit.tools'
 echo 'travis_fold:start:test.unit.node'
 
 # Run unit tests in node
-node ./dist/tools/tsc-watch/ node triggerCmds
+node ./dist/tools/tsc-watch/ node runCmdsOnly
 
 echo 'travis_fold:end:test.unit.node'
 