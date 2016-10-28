snedigqh/angular

commit b65fb6b245ee2e0b6191ba74e96f9c72cfed7feb

refactor(compiler): move host properties into DirectiveWrapper
Part of #11683

modules/@angular/compiler/src/directive_wrapper_compiler.ts
@@ -10,11 +10,19 @@ import {Injectable} from '@angular/core';
 
 import {CompileDirectiveMetadata, CompileIdentifierMetadata} from './compile_metadata';
 import {createCheckBindingField, createCheckBindingStmt} from './compiler_util/binding_util';
+import {convertPropertyBinding} from './compiler_util/expression_converter';
+import {writeToRenderer} from './compiler_util/render_util';
 import {CompilerConfig} from './config';
+import {Parser} from './expression_parser/parser';
 import {Identifiers, resolveIdentifier} from './identifiers';
+import {DEFAULT_INTERPOLATION_CONFIG} from './ml_parser/interpolation_config';
 import {ClassBuilder, createClassStmt} from './output/class_builder';
 import * as o from './output/output_ast';
-import {LifecycleHooks, isDefaultChangeDetectionStrategy} from './private_import_core';
+import {ParseError, ParseErrorLevel, ParseLocation, ParseSourceFile, ParseSourceSpan} from './parse_util';
+import {Console, LifecycleHooks, isDefaultChangeDetectionStrategy} from './private_import_core';
+import {ElementSchemaRegistry} from './schema/element_schema_registry';
+import {BindingParser} from './template_parser/binding_parser';
+import {BoundElementPropertyAst, BoundEventAst} from './template_parser/template_ast';
 
 export class DirectiveWrapperCompileResult {
   constructor(public statements: o.Statement[], public dirWrapperClassVar: string) {}
@@ -44,14 +52,28 @@ const RESET_CHANGES_STMT = o.THIS_EXPR.prop(CHANGES_FIELD_NAME).set(o.literalMap
 export class DirectiveWrapperCompiler {
   static dirWrapperClassName(id: CompileIdentifierMetadata) { return `Wrapper_${id.name}`; }
 
-  constructor(private compilerConfig: CompilerConfig) {}
+  constructor(
+      private compilerConfig: CompilerConfig, private _exprParser: Parser,
+      private _schemaRegistry: ElementSchemaRegistry, private _console: Console) {}
 
   compile(dirMeta: CompileDirectiveMetadata): DirectiveWrapperCompileResult {
     const builder = new DirectiveWrapperBuilder(this.compilerConfig, dirMeta);
     Object.keys(dirMeta.inputs).forEach((inputFieldName) => {
       addCheckInputMethod(inputFieldName, builder);
     });
-    addDetectChangesInternalMethod(builder);
+    addDetectChangesInInputPropsMethod(builder);
+
+    const hostParseResult = parseHostBindings(dirMeta, this._exprParser, this._schemaRegistry);
+    reportParseErrors(hostParseResult.errors, this._console);
+    // host properties are change detected by the DirectiveWrappers,
+    // except for the animation properties as they need close integration with animation events
+    // and DirectiveWrappers don't support
+    // event listeners right now.
+    addDetectChangesInHostPropsMethod(
+        hostParseResult.hostProps.filter(hostProp => !hostProp.isAnimation), builder);
+
+    // TODO(tbosch): implement hostListeners via DirectiveWrapper as well!
+
     const classStmt = builder.build();
     return new DirectiveWrapperCompileResult([classStmt], classStmt.name);
   }
@@ -108,7 +130,7 @@ class DirectiveWrapperBuilder implements ClassBuilder {
   }
 }
 
-function addDetectChangesInternalMethod(builder: DirectiveWrapperBuilder) {
+function addDetectChangesInInputPropsMethod(builder: DirectiveWrapperBuilder) {
   const changedVar = o.variable('changed');
   const stmts: o.Statement[] = [
     changedVar.set(o.THIS_EXPR.prop(CHANGED_FIELD_NAME)).toDeclStmt(),
@@ -148,7 +170,7 @@ function addDetectChangesInternalMethod(builder: DirectiveWrapperBuilder) {
   stmts.push(new o.ReturnStatement(changedVar));
 
   builder.methods.push(new o.ClassMethod(
-      'detectChangesInternal',
+      'detectChangesInInputProps',
       [
         new o.FnParam(
             VIEW_VAR.name, o.importType(resolveIdentifier(Identifiers.AppView), [o.DYNAMIC_TYPE])),
@@ -183,4 +205,73 @@ function addCheckInputMethod(input: string, builder: DirectiveWrapperBuilder) {
         new o.FnParam(FORCE_UPDATE_VAR.name, o.BOOL_TYPE),
       ],
       methodBody));
+}
+
+function addDetectChangesInHostPropsMethod(
+    hostProps: BoundElementPropertyAst[], builder: DirectiveWrapperBuilder) {
+  const stmts: o.Statement[] = [];
+  const methodParams: o.FnParam[] = [
+    new o.FnParam(
+        VIEW_VAR.name, o.importType(resolveIdentifier(Identifiers.AppView), [o.DYNAMIC_TYPE])),
+    new o.FnParam(RENDER_EL_VAR.name, o.DYNAMIC_TYPE),
+    new o.FnParam(THROW_ON_CHANGE_VAR.name, o.BOOL_TYPE),
+  ];
+  hostProps.forEach((hostProp) => {
+    const field = createCheckBindingField(builder);
+    const evalResult = convertPropertyBinding(
+        builder, null, o.THIS_EXPR.prop(CONTEXT_FIELD_NAME), hostProp.value, field.bindingId);
+    if (!evalResult) {
+      return;
+    }
+    let securityContextExpr: o.ReadVarExpr;
+    if (hostProp.needsRuntimeSecurityContext) {
+      securityContextExpr = o.variable(`secCtx_${methodParams.length}`);
+      methodParams.push(new o.FnParam(
+          securityContextExpr.name, o.importType(resolveIdentifier(Identifiers.SecurityContext))));
+    }
+    stmts.push(...createCheckBindingStmt(
+        evalResult, field.expression, THROW_ON_CHANGE_VAR,
+        writeToRenderer(
+            VIEW_VAR, hostProp, RENDER_EL_VAR, evalResult.currValExpr,
+            builder.compilerConfig.logBindingUpdate, securityContextExpr)));
+  });
+  builder.methods.push(new o.ClassMethod('detectChangesInHostProps', methodParams, stmts));
+}
+
+class ParseResult {
+  constructor(
+      public hostProps: BoundElementPropertyAst[], public hostListeners: BoundEventAst[],
+      public errors: ParseError[]) {}
+}
+
+function parseHostBindings(
+    dirMeta: CompileDirectiveMetadata, exprParser: Parser,
+    schemaRegistry: ElementSchemaRegistry): ParseResult {
+  const errors: ParseError[] = [];
+  const parser =
+      new BindingParser(exprParser, DEFAULT_INTERPOLATION_CONFIG, schemaRegistry, [], errors);
+  const sourceFileName = dirMeta.type.moduleUrl ?
+      `in Directive ${dirMeta.type.name} in ${dirMeta.type.moduleUrl}` :
+      `in Directive ${dirMeta.type.name}`;
+  const sourceFile = new ParseSourceFile('', sourceFileName);
+  const sourceSpan = new ParseSourceSpan(
+      new ParseLocation(sourceFile, null, null, null),
+      new ParseLocation(sourceFile, null, null, null));
+  const parsedHostProps = parser.createDirectiveHostPropertyAsts(dirMeta, sourceSpan);
+  const parsedHostListeners = parser.createDirectiveHostEventAsts(dirMeta, sourceSpan);
+
+  return new ParseResult(parsedHostProps, parsedHostListeners, errors);
+}
+
+function reportParseErrors(parseErrors: ParseError[], console: Console) {
+  const warnings = parseErrors.filter(error => error.level === ParseErrorLevel.WARNING);
+  const errors = parseErrors.filter(error => error.level === ParseErrorLevel.FATAL);
+
+  if (warnings.length > 0) {
+    this._console.warn(`Directive parse warnings:\n${warnings.join('\n')}`);
+  }
+
+  if (errors.length > 0) {
+    throw new Error(`Directive parse errors:\n${errors.join('\n')}`);
+  }
 }

modules/@angular/compiler-cli/src/codegen.ts
@@ -133,7 +133,9 @@ export class CodeGenerator {
     // TODO(vicb): do not pass cliOptions.i18nFormat here
     const offlineCompiler = new compiler.OfflineCompiler(
         resolver, normalizer, tmplParser, new compiler.StyleCompiler(urlResolver),
-        new compiler.ViewCompiler(config), new compiler.DirectiveWrapperCompiler(config),
+        new compiler.ViewCompiler(config, elementSchemaRegistry),
+        new compiler.DirectiveWrapperCompiler(
+            config, expressionParser, elementSchemaRegistry, console),
         new compiler.NgModuleCompiler(), new compiler.TypeScriptEmitter(reflectorHost),
         cliOptions.locale, cliOptions.i18nFormat);
 

modules/@angular/compiler/index.ts
@@ -34,7 +34,7 @@ export {DirectiveResolver} from './src/directive_resolver';
 export {PipeResolver} from './src/pipe_resolver';
 export {NgModuleResolver} from './src/ng_module_resolver';
 export {DEFAULT_INTERPOLATION_CONFIG, InterpolationConfig} from './src/ml_parser/interpolation_config';
-export {ElementSchemaRegistry} from './src/schema/element_schema_registry';
+export * from './src/schema/element_schema_registry';
 export * from './src/i18n/index';
 export * from './src/template_parser/template_ast';
 export * from './src/directive_normalizer';

modules/@angular/compiler/src/compiler_util/identifier_util.ts
@@ -8,7 +8,7 @@
 
 import {CompileTokenMetadata} from '../compile_metadata';
 import {isPresent} from '../facade/lang';
-import {Identifiers, resolveIdentifier} from '../identifiers';
+import {IdentifierSpec, Identifiers, resolveEnumIdentifier, resolveIdentifier} from '../identifiers';
 import * as o from '../output/output_ast';
 
 export function createDiTokenExpression(token: CompileTokenMetadata): o.Expression {
@@ -48,4 +48,13 @@ export function createPureProxy(
   builder.ctorStmts.push(o.THIS_EXPR.prop(pureProxyProp.name)
                              .set(o.importExpr(resolveIdentifier(pureProxyId)).callFn([fn]))
                              .toStmt());
+}
+
+export function createEnumExpression(enumType: IdentifierSpec, enumValue: any): o.Expression {
+  const enumName =
+      Object.keys(enumType.runtime).find((propName) => enumType.runtime[propName] === enumValue);
+  if (!enumName) {
+    throw new Error(`Unknown enum value ${enumValue} in ${enumType.name}`);
+  }
+  return o.importExpr(resolveEnumIdentifier(resolveIdentifier(enumType), enumName));
 }

modules/@angular/compiler/src/compiler_util/render_util.ts
@@ -12,23 +12,27 @@ import {Identifiers, resolveIdentifier} from '../identifiers';
 import * as o from '../output/output_ast';
 import {BoundElementPropertyAst, PropertyBindingType} from '../template_parser/template_ast';
 
+import {createEnumExpression} from './identifier_util';
+
 export function writeToRenderer(
-    view: o.Expression, boundProp: BoundElementPropertyAst, renderNode: o.Expression,
-    renderValue: o.Expression, logBindingUpdate: boolean): o.Statement[] {
+    view: o.Expression, boundProp: BoundElementPropertyAst, renderElement: o.Expression,
+    renderValue: o.Expression, logBindingUpdate: boolean,
+    securityContextExpression?: o.Expression): o.Statement[] {
   const updateStmts: o.Statement[] = [];
   const renderer = view.prop('renderer');
-  renderValue = sanitizedValue(view, boundProp, renderValue);
+  renderValue = sanitizedValue(view, boundProp, renderValue, securityContextExpression);
   switch (boundProp.type) {
     case PropertyBindingType.Property:
       if (logBindingUpdate) {
-        updateStmts.push(o.importExpr(resolveIdentifier(Identifiers.setBindingDebugInfo))
-                             .callFn([renderer, renderNode, o.literal(boundProp.name), renderValue])
-                             .toStmt());
+        updateStmts.push(
+            o.importExpr(resolveIdentifier(Identifiers.setBindingDebugInfo))
+                .callFn([renderer, renderElement, o.literal(boundProp.name), renderValue])
+                .toStmt());
       }
       updateStmts.push(
           renderer
               .callMethod(
-                  'setElementProperty', [renderNode, o.literal(boundProp.name), renderValue])
+                  'setElementProperty', [renderElement, o.literal(boundProp.name), renderValue])
               .toStmt());
       break;
     case PropertyBindingType.Attribute:
@@ -37,13 +41,14 @@ export function writeToRenderer(
       updateStmts.push(
           renderer
               .callMethod(
-                  'setElementAttribute', [renderNode, o.literal(boundProp.name), renderValue])
+                  'setElementAttribute', [renderElement, o.literal(boundProp.name), renderValue])
               .toStmt());
       break;
     case PropertyBindingType.Class:
       updateStmts.push(
           renderer
-              .callMethod('setElementClass', [renderNode, o.literal(boundProp.name), renderValue])
+              .callMethod(
+                  'setElementClass', [renderElement, o.literal(boundProp.name), renderValue])
               .toStmt());
       break;
     case PropertyBindingType.Style:
@@ -55,7 +60,8 @@ export function writeToRenderer(
       renderValue = renderValue.isBlank().conditional(o.NULL_EXPR, strValue);
       updateStmts.push(
           renderer
-              .callMethod('setElementStyle', [renderNode, o.literal(boundProp.name), renderValue])
+              .callMethod(
+                  'setElementStyle', [renderElement, o.literal(boundProp.name), renderValue])
               .toStmt());
       break;
     case PropertyBindingType.Animation:
@@ -65,32 +71,19 @@ export function writeToRenderer(
 }
 
 function sanitizedValue(
-    view: o.Expression, boundProp: BoundElementPropertyAst,
-    renderValue: o.Expression): o.Expression {
-  let enumValue: string;
-  switch (boundProp.securityContext) {
-    case SecurityContext.NONE:
-      return renderValue;  // No sanitization needed.
-    case SecurityContext.HTML:
-      enumValue = 'HTML';
-      break;
-    case SecurityContext.STYLE:
-      enumValue = 'STYLE';
-      break;
-    case SecurityContext.SCRIPT:
-      enumValue = 'SCRIPT';
-      break;
-    case SecurityContext.URL:
-      enumValue = 'URL';
-      break;
-    case SecurityContext.RESOURCE_URL:
-      enumValue = 'RESOURCE_URL';
-      break;
-    default:
-      throw new Error(`internal error, unexpected SecurityContext ${boundProp.securityContext}.`);
+    view: o.Expression, boundProp: BoundElementPropertyAst, renderValue: o.Expression,
+    securityContextExpression?: o.Expression): o.Expression {
+  if (boundProp.securityContext === SecurityContext.NONE) {
+    return renderValue;  // No sanitization needed.
+  }
+  if (!boundProp.needsRuntimeSecurityContext) {
+    securityContextExpression =
+        createEnumExpression(Identifiers.SecurityContext, boundProp.securityContext);
+  }
+  if (!securityContextExpression) {
+    throw new Error(`internal error, no SecurityContext given ${boundProp.name}`);
   }
   let ctx = view.prop('viewUtils').prop('sanitizer');
-  let args =
-      [o.importExpr(resolveIdentifier(Identifiers.SecurityContext)).prop(enumValue), renderValue];
+  let args = [securityContextExpression, renderValue];
   return ctx.callMethod('sanitize', args);
 }

modules/@angular/compiler/src/schema/dom_element_schema_registry.ts
@@ -328,7 +328,12 @@ export class DomElementSchemaRegistry extends ElementSchemaRegistry {
    * 'NONE' security context, i.e. that they are safe inert string values. Only specific well known
    * attack vectors are assigned their appropriate context.
    */
-  securityContext(tagName: string, propName: string): SecurityContext {
+  securityContext(tagName: string, propName: string, isAttribute: boolean): SecurityContext {
+    if (isAttribute) {
+      // NB: For security purposes, use the mapped property name, not the attribute name.
+      propName = this.getMappedPropName(propName);
+    }
+
     // Make sure comparisons are case insensitive, so that case differences between attribute and
     // property names do not have a security impact.
     tagName = tagName.toLowerCase();
@@ -366,4 +371,6 @@ export class DomElementSchemaRegistry extends ElementSchemaRegistry {
       return {error: false};
     }
   }
+
+  allKnownElementNames(): string[] { return Object.keys(this._schema); }
 }

modules/@angular/compiler/src/schema/element_schema_registry.ts
@@ -6,12 +6,14 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {SchemaMetadata} from '@angular/core';
+import {SchemaMetadata, SecurityContext} from '@angular/core';
 
 export abstract class ElementSchemaRegistry {
   abstract hasProperty(tagName: string, propName: string, schemaMetas: SchemaMetadata[]): boolean;
   abstract hasElement(tagName: string, schemaMetas: SchemaMetadata[]): boolean;
-  abstract securityContext(tagName: string, propName: string): any;
+  abstract securityContext(elementName: string, propName: string, isAttribute: boolean):
+      SecurityContext;
+  abstract allKnownElementNames(): string[];
   abstract getMappedPropName(propName: string): string;
   abstract getDefaultComponentElementName(): string;
   abstract validateProperty(name: string): {error: boolean, msg?: string};

commit 2e963ed2f1c350ff086b7774f0d15b80f76a8278

fix(compiler): don’t double bind functions
This fixes a performance regressions introduced by b65fb6b245ee2e0b6191ba74e96f9c72cfed7feb.

Also makes properties in the directive wrapper private
so that closure compiler can minify them better.

modules/@angular/compiler/src/directive_wrapper_compiler.ts
@@ -29,9 +29,9 @@ export class DirectiveWrapperCompileResult {
 }
 
 const CONTEXT_FIELD_NAME = 'context';
-const CHANGES_FIELD_NAME = 'changes';
-const CHANGED_FIELD_NAME = 'changed';
-const EVENT_HANDLER_FIELD_NAME = 'eventHandler';
+const CHANGES_FIELD_NAME = '_changes';
+const CHANGED_FIELD_NAME = '_changed';
+const EVENT_HANDLER_FIELD_NAME = '_eventHandler';
 
 const CURR_VALUE_VAR = o.variable('currValue');
 const THROW_ON_CHANGE_VAR = o.variable('throwOnChange');
@@ -129,14 +129,15 @@ class DirectiveWrapperBuilder implements ClassBuilder {
 
 
     const fields: o.ClassField[] = [
-      new o.ClassField(EVENT_HANDLER_FIELD_NAME, o.FUNCTION_TYPE),
+      new o.ClassField(EVENT_HANDLER_FIELD_NAME, o.FUNCTION_TYPE, [o.StmtModifier.Private]),
       new o.ClassField(CONTEXT_FIELD_NAME, o.importType(this.dirMeta.type)),
-      new o.ClassField(CHANGED_FIELD_NAME, o.BOOL_TYPE),
+      new o.ClassField(CHANGED_FIELD_NAME, o.BOOL_TYPE, [o.StmtModifier.Private]),
     ];
     const ctorStmts: o.Statement[] =
         [o.THIS_EXPR.prop(CHANGED_FIELD_NAME).set(o.literal(false)).toStmt()];
     if (this.genChanges) {
-      fields.push(new o.ClassField(CHANGES_FIELD_NAME, new o.MapType(o.DYNAMIC_TYPE)));
+      fields.push(new o.ClassField(
+          CHANGES_FIELD_NAME, new o.MapType(o.DYNAMIC_TYPE), [o.StmtModifier.Private]));
       ctorStmts.push(RESET_CHANGES_STMT);
     }
 
@@ -303,7 +304,11 @@ function addHandleEventMethod(hostListeners: BoundEventAst[], builder: Directive
 }
 
 function addSubscribeMethod(dirMeta: CompileDirectiveMetadata, builder: DirectiveWrapperBuilder) {
-  const methodParams: o.FnParam[] = [new o.FnParam(EVENT_HANDLER_FIELD_NAME, o.DYNAMIC_TYPE)];
+  const methodParams: o.FnParam[] = [
+    new o.FnParam(
+        VIEW_VAR.name, o.importType(resolveIdentifier(Identifiers.AppView), [o.DYNAMIC_TYPE])),
+    new o.FnParam(EVENT_HANDLER_FIELD_NAME, o.DYNAMIC_TYPE)
+  ];
   const stmts: o.Statement[] = [
     o.THIS_EXPR.prop(EVENT_HANDLER_FIELD_NAME).set(o.variable(EVENT_HANDLER_FIELD_NAME)).toStmt()
   ];
@@ -313,17 +318,16 @@ function addSubscribeMethod(dirMeta: CompileDirectiveMetadata, builder: Directiv
     methodParams.push(new o.FnParam(paramName, o.BOOL_TYPE));
     const subscriptionFieldName = `subscription${emitterIdx}`;
     builder.fields.push(new o.ClassField(subscriptionFieldName, o.DYNAMIC_TYPE));
-    stmts.push(new o.IfStmt(
-        o.variable(paramName),
-        [o.THIS_EXPR.prop(subscriptionFieldName)
-             .set(o.THIS_EXPR.prop(CONTEXT_FIELD_NAME)
-                      .prop(emitterPropName)
-                      .callMethod(
-                          o.BuiltinMethod.SubscribeObservable,
-                          [o.variable(EVENT_HANDLER_FIELD_NAME)
-                               .callMethod(
-                                   o.BuiltinMethod.Bind, [o.NULL_EXPR, o.literal(eventName)])]))
-             .toStmt()]));
+    stmts.push(new o.IfStmt(o.variable(paramName), [
+      o.THIS_EXPR.prop(subscriptionFieldName)
+          .set(o.THIS_EXPR.prop(CONTEXT_FIELD_NAME)
+                   .prop(emitterPropName)
+                   .callMethod(
+                       o.BuiltinMethod.SubscribeObservable,
+                       [o.variable(EVENT_HANDLER_FIELD_NAME)
+                            .callMethod(o.BuiltinMethod.Bind, [VIEW_VAR, o.literal(eventName)])]))
+          .toStmt()
+    ]));
     builder.destroyStmts.push(
         o.THIS_EXPR.prop(subscriptionFieldName)
             .and(o.THIS_EXPR.prop(subscriptionFieldName).callMethod('unsubscribe', []))
@@ -424,7 +428,7 @@ export class DirectiveWrapperExpressions {
   }
   static subscribe(
       dirMeta: CompileDirectiveMetadata, hostProps: BoundElementPropertyAst[], usedEvents: string[],
-      dirWrapper: o.Expression, eventListener: o.Expression): o.Statement[] {
+      dirWrapper: o.Expression, view: o.Expression, eventListener: o.Expression): o.Statement[] {
     let needsSubscribe = false;
     let eventFlags: o.Expression[] = [];
     Object.keys(dirMeta.outputs).forEach((propName) => {
@@ -439,7 +443,9 @@ export class DirectiveWrapperExpressions {
       }
     });
     if (needsSubscribe) {
-      return [dirWrapper.callMethod('subscribe', [eventListener].concat(eventFlags)).toStmt()];
+      return [
+        dirWrapper.callMethod('subscribe', [view, eventListener].concat(eventFlags)).toStmt()
+      ];
     } else {
       return [];
     }

modules/@angular/compiler/src/view_compiler/event_binder.ts
@@ -59,8 +59,8 @@ function subscribeToRenderEvents(
     compileElement.view.createMethod.addStmt(
         disposableVar
             .set(o.importExpr(resolveIdentifier(Identifiers.subscribeToRenderElement)).callFn([
-              ViewProperties.renderer, compileElement.renderNode,
-              createInlineArray(eventAndTargetExprs), handleEventClosure(compileElement)
+              o.THIS_EXPR, compileElement.renderNode, createInlineArray(eventAndTargetExprs),
+              handleEventExpr(compileElement)
             ]))
             .toDeclStmt(o.FUNCTION_TYPE, [o.StmtModifier.Private]));
   }
@@ -73,8 +73,8 @@ function subscribeToDirectiveEvents(
   directives.forEach((dirAst) => {
     const dirWrapper = compileElement.directiveWrapperInstance.get(dirAst.directive.type.reference);
     compileElement.view.createMethod.addStmts(DirectiveWrapperExpressions.subscribe(
-        dirAst.directive, dirAst.hostProperties, usedEventNames, dirWrapper,
-        handleEventClosure(compileElement)));
+        dirAst.directive, dirAst.hostProperties, usedEventNames, dirWrapper, o.THIS_EXPR,
+        handleEventExpr(compileElement)));
   });
 }
 
@@ -127,11 +127,9 @@ function generateHandleEventMethod(
       handleEventStmts.finish(), o.BOOL_TYPE));
 }
 
-function handleEventClosure(compileElement: CompileElement) {
+function handleEventExpr(compileElement: CompileElement) {
   const handleEventMethodName = getHandleEventMethodName(compileElement.nodeIndex);
-  return o.THIS_EXPR.callMethod(
-      'eventHandler',
-      [o.THIS_EXPR.prop(handleEventMethodName).callMethod(o.BuiltinMethod.Bind, [o.THIS_EXPR])]);
+  return o.THIS_EXPR.callMethod('eventHandler', [o.THIS_EXPR.prop(handleEventMethodName)]);
 }
 
 type EventSummary = {

modules/@angular/core/src/linker/view.ts
@@ -375,7 +375,7 @@ export class DebugAppView<T> extends AppView<T> {
     return (eventName: string, event?: any) => {
       this._resetDebug();
       try {
-        return superHandler(eventName, event);
+        return superHandler.call(this, eventName, event);
       } catch (e) {
         this._rethrowWithContext(e);
         throw e;

modules/@angular/core/src/linker/view_utils.ts
@@ -17,6 +17,7 @@ import {Sanitizer} from '../security';
 
 import {AppElement} from './element';
 import {ExpressionChangedAfterItHasBeenCheckedError} from './errors';
+import {AppView} from './view';
 
 @Injectable()
 export class ViewUtils {
@@ -401,7 +402,7 @@ export function selectOrCreateRenderHostElement(
 }
 
 export function subscribeToRenderElement(
-    renderer: Renderer, element: any, eventNamesAndTargets: InlineArray<string>,
+    view: AppView<any>, element: any, eventNamesAndTargets: InlineArray<string>,
     listener: (eventName: string, event: any) => any) {
   const disposables = createEmptyInlineArray(eventNamesAndTargets.length / 2);
   for (var i = 0; i < eventNamesAndTargets.length; i += 2) {
@@ -409,10 +410,10 @@ export function subscribeToRenderElement(
     const eventTarget = eventNamesAndTargets.get(i + 1);
     let disposable: Function;
     if (eventTarget) {
-      disposable = renderer.listenGlobal(
-          eventTarget, eventName, listener.bind(null, `${eventTarget}:${eventName}`));
+      disposable = view.renderer.listenGlobal(
+          eventTarget, eventName, listener.bind(view, `${eventTarget}:${eventName}`));
     } else {
-      disposable = renderer.listen(element, eventName, listener.bind(null, eventName));
+      disposable = view.renderer.listen(element, eventName, listener.bind(view, eventName));
     }
     disposables.set(i / 2, disposable);
   }