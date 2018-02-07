snedigqh/angular

commit d7b848207e874c445a011a45602ab4b1e81bfde5

refactor(core): Removed readonly getters and changed to readonly (#19842) variables
PR Close #19842

packages/core/src/linker/query_list.ts
@@ -41,9 +41,9 @@ export class QueryList<T>/* implements Iterable<T> */ {
   private _results: Array<T> = [];
   public readonly changes: Observable<any> = new EventEmitter();
 
-  get length(): number { return this._results.length; }
-  get first(): T { return this._results[0]; }
-  get last(): T { return this._results[this.length - 1]; }
+  readonly length: number;
+  readonly first: T;
+  readonly last: T;
 
   /**
    * See
@@ -98,6 +98,9 @@ export class QueryList<T>/* implements Iterable<T> */ {
   reset(res: Array<T|any[]>): void {
     this._results = flatten(res);
     (this as{dirty: boolean}).dirty = false;
+    (this as{length: number}).length = this._results.length;
+    (this as{last: T}).last = this._results[this.length - 1];
+    (this as{first: T}).first = this._results[0];
   }
 
   notifyOnChanges(): void { (this.changes as EventEmitter<any>).emit(this); }

packages/animations/browser/src/render/transition_animation_engine.ts
@@ -1389,9 +1389,9 @@ export class TransitionAnimationPlayer implements AnimationPlayer {
 
   public markedForDestroy: boolean = false;
 
-  constructor(public namespaceId: string, public triggerName: string, public element: any) {}
+  readonly queued: boolean = true;
 
-  get queued() { return this._containsRealPlayer == false; }
+  constructor(public namespaceId: string, public triggerName: string, public element: any) {}
 
   setRealPlayer(player: AnimationPlayer) {
     if (this._containsRealPlayer) return;
@@ -1403,6 +1403,7 @@ export class TransitionAnimationPlayer implements AnimationPlayer {
     });
     this._queuedCallbacks = {};
     this._containsRealPlayer = true;
+    (this as{queued: boolean}).queued = false;
   }
 
   getRealPlayer() { return this._player; }

packages/common/http/test/jsonp_mock.ts
@@ -23,12 +23,11 @@ export class MockScriptElement {
 
 export class MockDocument {
   mock: MockScriptElement|null;
+  readonly body: any = this;
 
   createElement(tag: 'script'): HTMLScriptElement {
     return new MockScriptElement() as any as HTMLScriptElement;
   }
-
-  get body(): any { return this; }
 
   appendChild(node: any): void { this.mock = node; }
 

packages/core/src/di/reflective_provider.ts
@@ -71,11 +71,13 @@ export interface ResolvedReflectiveProvider {
 }
 
 export class ResolvedReflectiveProvider_ implements ResolvedReflectiveProvider {
+  readonly resolvedFactory: ResolvedReflectiveFactory;
+
   constructor(
       public key: ReflectiveKey, public resolvedFactories: ResolvedReflectiveFactory[],
-      public multiProvider: boolean) {}
-
-  get resolvedFactory(): ResolvedReflectiveFactory { return this.resolvedFactories[0]; }
+      public multiProvider: boolean) {
+    this.resolvedFactory = this.resolvedFactories[0];
+  }
 }
 
 /**

packages/core/src/linker/component_factory_resolver.ts
@@ -66,13 +66,20 @@ export class CodegenComponentFactoryResolver implements ComponentFactoryResolver
 }
 
 export class ComponentFactoryBoundToModule<C> extends ComponentFactory<C> {
-  constructor(private factory: ComponentFactory<C>, private ngModule: NgModuleRef<any>) { super(); }
+  readonly selector: string;
+  readonly componentType: Type<any>;
+  readonly ngContentSelectors: string[];
+  readonly inputs: {propName: string, templateName: string}[];
+  readonly outputs: {propName: string, templateName: string}[];
 
-  get selector() { return this.factory.selector; }
-  get componentType() { return this.factory.componentType; }
-  get ngContentSelectors() { return this.factory.ngContentSelectors; }
-  get inputs() { return this.factory.inputs; }
-  get outputs() { return this.factory.outputs; }
+  constructor(private factory: ComponentFactory<C>, private ngModule: NgModuleRef<any>) {
+    super();
+    this.selector = factory.selector;
+    this.componentType = factory.componentType;
+    this.ngContentSelectors = factory.ngContentSelectors;
+    this.inputs = factory.inputs;
+    this.outputs = factory.outputs;
+  }
 
   create(
       injector: Injector, projectableNodes?: any[][], rootSelectorOrNode?: string|any,

packages/core/src/view/refs.ts
@@ -481,6 +481,8 @@ class NgModuleRef_ implements NgModuleData, InternalNgModuleRef<any> {
   /** @internal */
   _providers: any[];
 
+  readonly injector: Injector = this;
+
   constructor(
       private _moduleType: Type<any>, public _parent: Injector,
       public _bootstrapComponents: Type<any>[], public _def: NgModuleDefinition) {
@@ -495,8 +497,6 @@ class NgModuleRef_ implements NgModuleData, InternalNgModuleRef<any> {
   get instance() { return this.get(this._moduleType); }
 
   get componentFactoryResolver() { return this.get(ComponentFactoryResolver); }
-
-  get injector(): Injector { return this; }
 
   destroy(): void {
     if (this._destroyed) {

packages/core/src/view/services.ts
@@ -649,9 +649,8 @@ class DebugRendererFactory2 implements RendererFactory2 {
 
 
 class DebugRenderer2 implements Renderer2 {
-  constructor(private delegate: Renderer2) {}
-
-  get data() { return this.delegate.data; }
+  readonly data: {[key: string]: any};
+  constructor(private delegate: Renderer2) { this.data = this.delegate.data; }
 
   destroyNode(node: any) {
     removeDebugNodeFromIndex(getDebugNode(node) !);

packages/router/src/pre_activation.ts
@@ -26,8 +26,10 @@ import {andObservables, forEach, shallowEqual, wrapIntoObservable} from './utils
 import {TreeNode, nodeChildrenAsMap} from './utils/tree';
 
 class CanActivate {
-  constructor(public path: ActivatedRouteSnapshot[]) {}
-  get route(): ActivatedRouteSnapshot { return this.path[this.path.length - 1]; }
+  readonly route: ActivatedRouteSnapshot;
+  constructor(public path: ActivatedRouteSnapshot[]) {
+    this.route = this.path[this.path.length - 1];
+  }
 }
 
 class CanDeactivate {

commit 93520e1d70b6eafbf9d021e5a528702af9c7dd23

fix(core): ensure initial value of QueryList length (#21980) (#21982)
Set initial value of `length` to `0`.

Fixes regression introduced by https://github.com/angular/angular/commit/d7b848207e874c445a011a45602ab4b1e81bfde5#diff-a85dbe0991a7577ea24b49374e9ae90b where the `length` property ceased to have initial value.

Closes #21980

PR Close #21982

packages/core/src/linker/query_list.ts
@@ -41,7 +41,7 @@ export class QueryList<T>/* implements Iterable<T> */ {
   private _results: Array<T> = [];
   public readonly changes: Observable<any> = new EventEmitter();
 
-  readonly length: number;
+  readonly length: number = 0;
   readonly first: T;
   readonly last: T;
 

packages/core/test/linker/query_list_spec.ts
@@ -23,6 +23,22 @@ import {getDOM} from '@angular/platform-browser/src/dom/dom_adapter';
 
     function logAppend(item: any /** TODO #9100 */) { log += (log.length == 0 ? '' : ', ') + item; }
 
+    describe('dirty and reset', () => {
+
+      it('should initially be dirty and empty', () => {
+        expect(queryList.dirty).toBeTruthy();
+        expect(queryList.length).toBe(0);
+      });
+
+      it('should be not dirty after reset', () => {
+        expect(queryList.dirty).toBeTruthy();
+        queryList.reset(['one', 'two']);
+        expect(queryList.dirty).toBeFalsy();
+        expect(queryList.length).toBe(2);
+      });
+
+    });
+
     it('should support resetting and iterating over the new objects', () => {
       queryList.reset(['one']);
       queryList.reset(['two']);